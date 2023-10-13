4b87ea37bb77c9e27f4b6d48393417905d1cc92c
Alejandro Perez Fadon <ap3222@ic.ac.uk>
2023-10-13 14:31:44 +0100

    Revert "[ap3222] Made update_recent_cpu,implemented thread_get_load_avg() and thread_get_recent_cpu. Also fixed compile errors in threads.c"
    This reverts commit c73f4ea3a39d65d822f30a69720fd12676785afc.

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -29,12 +29,18 @@
 // Takes in an FP and one int and returns FP
 #define ADD_FP_AND_INT(X, N) ((X) + (INT_TO_FP(N))) 
 
+<<<<<<< HEAD
 // Takes in 2 FP and returns FP
 #define MULT_FPS(X, Y) (((int64_t) (X) * (Y)) / FP_F) 
 
+=======
 // Takes in an FP and one int and returns FP
 #define SUB_FP_AND_INT(X, N) ((X) - (INT_TO_FP(N)))
 
+// Takes in 2 FP and returns FP
+#define MULT_FPS(X, Y) (((int64_t) (X) * (Y)) / FP_F) 
+
+>>>>>>> c1c9ffc50f2fbc567825f55ee1ecb02e37e26d76
 // Takes in 1 FP and one int and returns FP
 #define MULT_INT_TO_FP(X, N) ((X) * (N)) 
 
