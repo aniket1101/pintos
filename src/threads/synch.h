ef803a693c8f495ed8371601891c825f416ea2d6
Adam Watson <aw1122@ic.ac.uk>
2023-10-17 22:24:15 +0100

    [aw1122][jw5022] - Added list_elem and effective priority member to lock struct

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,8 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem elem;      /* Allows list of locks*/
+    int eff_pri;           /* Holds highest priority of all waiting threads*/
   };
 
 void lock_init (struct lock *);
