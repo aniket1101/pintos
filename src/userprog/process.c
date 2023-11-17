7ea7bfde908565bc7fb8c8a1f992da2c1fba7765
Adam Watson <aw1122@ic.ac.uk>
2023-11-17 16:43:28 +0000

    Fixed wait so a parent will not call sema down if the child exit code is already available

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -233,8 +233,10 @@ process_wait (tid_t child_tid)
     }
   }
 
-  // Waits for child to terminate/exit
-  sema_down(&link->waiter);
+    // Checks if child exit code is already available, if not, call sema_down
+  if (link->child_alive) {
+    sema_down(&link->waiter);
+  }
 
   // Removes from the hash and free's pc_link struct as wait has completed
   int exit_code = link->child_exit_code;
