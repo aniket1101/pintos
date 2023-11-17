161fdea3642a8082d588fd0a17354e11b5597f14
Adam Watson <aw1122@ic.ac.uk>
2023-11-17 14:56:38 +0000

    Added function comments, removed redundant locks in pc_link_kill_child and updated pc_link_lookup to use hash_find

--- src/userprog/pc_link.c
+++ src/userprog/pc_link.c
@@ -13,23 +13,31 @@ static bool pc_link_less(const struct hash_elem *a,
 
 static unsigned pc_link_hash(const struct hash_elem *e, void *aux UNUSED);
 
+/* Initialises the pc_link system, creating the hash to contain the
+   pc links and the lock for mod*/
 void pc_link_system_init(void) {
   hash_init(&pc_link_hash_table, &pc_link_hash, &pc_link_less, NULL);
   lock_init(&pc_link_lock);
 }
 
+/* Less function to compare 2 pc links on the child thread id. */
 static bool pc_link_less(const struct hash_elem *a, 
     const struct hash_elem *b, void *aux UNUSED) {
   return hash_entry(a, struct pc_link, elem)->child_tid
     < hash_entry(b, struct pc_link, elem)->child_tid;
 }
 
+/* Hash function to get the key of the hash, as the child tid will 
+   be unique, we can use this. */
 static unsigned pc_link_hash(const struct hash_elem *e, void *aux UNUSED) {
   return hash_entry(e, struct pc_link, elem)->child_tid;
 }
 
+/* Initialises the pc_link struct, returns the link if successful,
+   and exits if unsuccessful. */
 struct pc_link *pc_link_init(tid_t child_tid) {
   struct pc_link *link = (struct pc_link *) malloc (sizeof(struct pc_link));
+
   if (link == NULL) {
     kernel_exit(-1);
   }
@@ -46,51 +54,62 @@ struct pc_link *pc_link_init(tid_t child_tid) {
   return link;
 }
 
+/* Given a child tid, it will attempt to retrieve the pc_link struct 
+   in the pc_link hash table which has the corresponding child tid.
+   Returns the pc_link pointer if successful and NULL if unsuccessful. */
 struct pc_link *pc_link_lookup(int child_tid) {
   lock_acquire(&pc_link_lock);
-  if (!hash_empty(&pc_link_hash_table)) {
-    struct hash_iterator i;
 
-    hash_first (&i, &pc_link_hash_table);
-    while (hash_next (&i)) {
-      struct pc_link *link = hash_entry (hash_cur (&i), struct pc_link, elem);
-      if (link->child_tid == child_tid) {
-        lock_release(&pc_link_lock);
-        return link;
-      }
-    }
-  }
+  struct pc_link fake_link = {.child_tid = child_tid};
+  struct hash_elem *found_elem 
+    = hash_find(&pc_link_hash_table, &(fake_link.elem));
+  struct pc_link *link 
+    = found_elem != NULL ? hash_entry(found_elem, struct pc_link, elem) : NULL;
 
   lock_release(&pc_link_lock);
-  return NULL;
+  return link;
 }
 
+/* Given a child thread, it will attempt to find a corresponding pc_link struct
+   using the threads tid, if it finds this, it then updates the pc_link struct
+   with the information that the child thread is dead and supplies it's exit 
+   code, sema_up is used to allow the waiting parent to continue execution. */
 void pc_link_kill_child(struct thread *child) {
   struct pc_link *link = pc_link_lookup(child->tid);
+
   if (link != NULL) {
-    lock_acquire(&pc_link_lock);
     link->child_exit_code = child->exit_code;
     link->child_alive = false;
-    lock_release(&pc_link_lock);
-
     sema_up(&link->waiter);
   }
 }
 
+/* Given a pc_link struct pointer, this will attempt it from the pc_link 
+   hash table. */
 struct pc_link *pc_link_remove(struct pc_link *link) {
   lock_acquire(&pc_link_lock);
 
-  struct hash_elem *removed_elem = hash_delete(&pc_link_hash_table, &link->elem);
-  struct pc_link *removed_link = removed_elem != NULL ? hash_entry (removed_elem, struct pc_link, elem) : NULL;
+  struct hash_elem *removed_elem 
+    = hash_delete(&pc_link_hash_table, &link->elem);
+  struct pc_link *removed_link 
+    = removed_elem != NULL ? 
+      hash_entry (removed_elem, struct pc_link, elem) : NULL;
   
   lock_release(&pc_link_lock);
   return removed_link;
 }
 
+/* A function given for the pc_link hash table's destroy function,
+   this allows the hash elements to be free'd when the hash is removed. */
 void pc_link_free(struct hash_elem *elem, void *aux UNUSED) {
   free(hash_entry (elem, struct pc_link, elem));
 }
 
+/* This will be called when a thread exits, it will check all hash elements
+   to see if the given tid matches the parent tid in the pc_link. If so
+   this needs to be removed from the hash and free'd. To do this, we store the 
+   elements to be removed in an array and remove them after to allow the hash
+   to iterate through properly, as removal during the iterator would stop it.*/
 void pc_link_free_parents(int parent_tid) {
   struct pc_link *to_remove[hash_size(&pc_link_hash_table)];
   int index = 0;
@@ -116,6 +135,8 @@ void pc_link_free_parents(int parent_tid) {
   }
 }
 
+/* Function given in hash_destroy for the pc_link hash table to free all 
+   elements in the hash. */
 void pc_link_hash_destroy(void) {
   hash_destroy(&pc_link_hash_table, &pc_link_free);
 }
