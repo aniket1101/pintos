6f7d386c49b819a2601f7e3dd464f0dffbd24711
aniket1101 <1.aniketgupta@gmail.com>
2023-11-21 20:26:59 +0000

    Made skeletons for mmap and munmap in syscall.c and began a little of the implementation

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -83,6 +83,8 @@ static inline int kernel_open (const char *file);
 static inline int kernel_read (int fd, void *buffer, unsigned length);
 static inline int kernel_write (int fd, const void *buffer, unsigned length);
 static inline void kernel_close (int fd);
+static inline int kernel_mmap (void *addr, struct file_info *info);
+static inline void kernel_munmap (int mapping);
 
 void
 syscall_init (void) 
@@ -229,11 +231,15 @@ static void handle_close(struct intr_frame *f) {
 
 /* Wrapper for kernel_mmap() */
 static void handle_mmap(struct intr_frame *f) {
-
+  int fd_num = pop_arg(0, int);
+  void *addr = pop_arg(1, void *);
+  struct fd *fd_ = fd_lookup_safe(fd_num);
+  f->eax = kernel_mmap(addr, fd_->file_info);
 }
 
 static void handle_munmap(struct intr_frame *f) {
-  
+  int mapping = pop_arg(0, int);
+  kernel_munmap(mapping);
 }
 
 /* Below are implementations of syscall functions with kernel access */ 
@@ -438,6 +444,14 @@ static inline void kernel_close(int fd_num) {
 
     lock_release(&filesys_lock);
   }
+}
+
+static inline int kernel_mmap(void *addr, struct file_info *info) {
+
+}
+
+static inline void kernel_munmap(int mapping) {
+  
 }
 
 /* Acquires filesys lock. */
