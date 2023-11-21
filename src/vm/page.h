dff1b983b89ccca550d793b8ef15853efea0fb24
Aniket Gupta <ag2922@ic.ac.uk>
2023-11-21 11:59:17 +0000

    Changed status from int to enum page status, which was also implemented.

--- src/vm/page.h
+++ src/vm/page.h
@@ -5,9 +5,16 @@
 
 struct supp_page_table_elem {
     void *vaddr;                /* Virtual memory address for a page*/
-    int status;                 /* Status of a page*/
+    page_status status;         /* Status of a page*/
     struct hash_elem elem;      /* Allows for hash of pages*/
 };
+
+enum page_status {
+    LOADED,                     /* Loaded into memorty*/
+    SWAPPED,                    /* Swapped out*/
+    ZERO,                       /* Page completely zeroed out*/
+    LAZY                        /* Lazy loaded*/
+}
 
 bool supp_page_table_init(struct hash *hash_table);
 void supp_page_table_destroy(struct hash *hash_table);
