a5be0902720ec3234ff3794c40752a761e19ecdd
ag2922 <aniket.gupta22@imperial.ac.uk>
2023-11-29 15:47:01 +0000

    Revert "Merge branch 'frame-wrap-palloc' into 'master'"
    This reverts merge request !10

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -9,7 +9,8 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-#include "vm/frame.h"
+#include "threads/palloc.h"
+#include "vm/page.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -125,47 +126,76 @@ kill (struct intr_frame *f)
 static void
 page_fault (struct intr_frame *f) 
 {
-   bool not_present;  /* True: not-present page, false: writing r/o page. */
-   bool write;        /* True: access was write, false: access was read. */
-   bool user;         /* True: access by user, false: access by kernel. */
-   void *fault_addr;  /* Fault address. */
+  bool not_present;  /* True: not-present page, false: writing r/o page. */
+  bool write;        /* True: access was write, false: access was read. */
+  bool user;         /* True: access by user, false: access by kernel. */
+  void *fault_addr;  /* Fault address. */
 
-   /* Obtain faulting address, the virtual address that was
-      accessed to cause the fault.  It may point to code or to
-      data.  It is not necessarily the address of the instruction
-      that caused the fault (that's f->eip).
-      See [IA32-v2a] "MOV--Move to/from Control Registers" and
-      [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception
-      (#PF)". */
-   asm ("movl %%cr2, %0" : "=r" (fault_addr));
+  /* Obtain faulting address, the virtual address that was
+     accessed to cause the fault.  It may point to code or to
+     data.  It is not necessarily the address of the instruction
+     that caused the fault (that's f->eip).
+     See [IA32-v2a] "MOV--Move to/from Control Registers" and
+     [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception
+     (#PF)". */
+  asm ("movl %%cr2, %0" : "=r" (fault_addr));
 
-   /* Turn interrupts back on (they were only off so that we could
-      be assured of reading CR2 before it changed). */
-   intr_enable ();
+  /* Turn interrupts back on (they were only off so that we could
+     be assured of reading CR2 before it changed). */
+  intr_enable ();
 
-   /* Count page faults. */
-   page_fault_cnt++;
+  /* Count page faults. */
+  page_fault_cnt++;
 
-   /* Determine cause. */
-   not_present = (f->error_code & PF_P) == 0;
-   write = (f->error_code & PF_W) != 0;
-   user = (f->error_code & PF_U) != 0;
-	
-   if (user) { // If user access has faulted, kill user process
- 	   if (!is_user_vaddr(fault_addr) || fault_addr == NULL) {
-         kernel_exit(-1);
-      }
+  /* Determine cause. */
+  not_present = (f->error_code & PF_P) == 0;
+  write = (f->error_code & PF_W) != 0;
+  user = (f->error_code & PF_U) != 0;
+
+  /* To implement virtual memory, delete the rest of the function
+     body, and replace it with code that brings in the page to
+     which fault_addr refers. */
+
+   /* Check if a fault address is in kernel space. */
+   if(is_kernel_vaddr(fault_addr)) {
+      kernel_exit(-1);
    }
 
-	// Otherwise crash kernel
+   /* Makes the fault address start at a page boundary .*/
+   void *vaddr = pg_round_down(fault_addr);
 
-   /* To implement virtual memory, delete the rest of the function
-      body, and replace it with code that brings in the page to
-      which fault_addr refers. */
-   printf("Page fault at %p: %s error %s page in %s context.\n",
+   /* Frame's virtual address. */
+   void *frame_addr;
+
+   struct thread *curr = thread_current();
+   struct supp_page_table_elem *page = get_supp_page_table(&curr->supp_page_table, vaddr);
+
+   if(page == NULL) { /* Exit if the page does not exist. */
+      kernel_exit(-1);
+   } else {
+      switch (page->status)
+      {
+      case LOADED:
+         break;
+      case SWAPPED:
+         break;
+      case ZERO:
+         frame_addr = palloc_get_page(PAL_ZERO);
+         break;
+      case LAZY:
+         break;
+      default:
+         break;
+      }
+      // TO DO: Note that writable should not always be true
+      pagedir_set_page(curr->pagedir, vaddr, frame_addr, curr->is_writable);
+      page->status = LOADED;
+   }
+
+  printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
-   kill (f);
+  kill (f);
 }
