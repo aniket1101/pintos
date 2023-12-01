9de7d1cbf4296d4dc276943f73f777b1ccf4679c
Aniket Gupta <ag2922@ic.ac.uk>
2023-12-01 10:51:24 +0000

    Changed erroneous static in install_page in process.c

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -643,7 +643,7 @@ setup_stack (void **esp)
    with palloc_get_page().
    Returns true on success, false if UPAGE is already mapped or
    if memory allocation fails. */
-static bool
+bool
 install_page (void *upage, void *kpage, bool writable)
 {
   struct thread *t = thread_current ();
