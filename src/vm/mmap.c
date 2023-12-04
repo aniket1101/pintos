99c0f021acab7deb4bd8effa911fd274fdc716d7
Alejandro Perez Fadon <ap3222@ic.ac.uk>
2023-12-04 14:21:12 +0000

    [ap3222][ag2922] Made mmap_file and mmap_file_page struct

--- src/vm/mmap.c
+++ src/vm/mmap.c
@@ -1,1 +1,17 @@
 #include "mmap.h"
+
+bool mmap_table_init(struct hash *hash_table) {
+    return hash_init(hash_table, &mmap_table_hash,
+                                 &mmap_table_less, NULL);
+}
+
+static unsigned mmap_table_hash(const struct hash_elem *e, void *aux) {
+  struct mmap_file *entry = hash_entry(e, struct mmap_file, elem);
+  return hash_bytes(&entry->vaddr, sizeof(entry->vaddr));
+}
+
+static bool mmap_table_less(const struct hash_elem *a, 
+        const struct hash_elem *b, void *aux) {
+    return hash_entry(a, struct supp_page, elem)->vaddr
+    < hash_entry(b, struct supp_page, elem)->vaddr;
+}
--- src/vm/mmap.h
+++ src/vm/mmap.h
@@ -3,5 +3,20 @@
 typedef int mapid_t;
 
 struct mmap_file {
-
+    int fd;
+    int size;
+    void *start_page;
+    mapid_t map_id;
+    struct list page_list;
 };
+
+struct mmap_file_page {
+    mapid_t map_id;
+    void *page;
+    struct file *file;
+    off_t offset;
+    int page_space;
+    bool is_writable;
+    struct hash_elem h_elem;
+    struct list_elem l_elem;
+}
